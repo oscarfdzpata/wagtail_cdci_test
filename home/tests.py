16eb94b6c45c41f5adc53e9cc1468aae5f73df3d
Oscar Fernandez Pata <oscarfpata@gmail.com>
2022-07-15 17:37:28 +0200

    aseert

--- home/tests.py
+++ home/tests.py
@@ -8,5 +8,6 @@ from .models import HomePage,Slide
 class MyPageTests(WagtailPageTests):
     def test_can_create_under_home_page(self):
         # You can create a ContentPage under a HomePage
-        self.assertCanNotCreateAt(HomePage,Slide)
+        # self.assertCanNotCreateAt(HomePage,Slide)
+        self.assertCanCreateAt(HomePage,Slide)
 
