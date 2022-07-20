b5f833d54e45dd6d7c39bb85b58905db0ac9f934
Oscar Fernandez Pata <oscarfpata@gmail.com>
2022-07-20 10:34:20 +0200

    comment tkinter

--- userManagement/forms.py
+++ userManagement/forms.py
@@ -1,5 +1,5 @@
 from calendar import calendar
-from tkinter.tix import Form
+#from tkinter.tix import Form
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import  UserCreationForm 
